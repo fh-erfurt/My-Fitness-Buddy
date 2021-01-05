4555e1b69d824fdf00b63d84551894c11f3c58ba
BenjaminS01 <bswarovsky@gmx.net>
2021-01-05 20:43:25 +0100

    Training create spinner (@strings, @array), list_item_training, list_item_exerciese_withcheckbox bearbeitet, code aufgeräumt (noch nicht komplett)

---
+++ README.md
@@ -0,0 +1,27 @@
+![CodeFactor](https://img.shields.io/badge/Android%20Studio-4.1-green)
+
+# My-Fitness-Buddy
+Für das Modul <b>Programmierung mobiler Endgeräte</b> entwickeln wir in unserem Team eine Fitness App für die Erstellung und Verwendung von individuellen Trainingsplänen.
+
+### Projektteam
+* **Benjamin Swarovsky** - [Profil](https://github.com/BenjaminS01)
+* **Christian König** - [Profil](https://github.com/christiankoenig)
+* **Marco Petzold** - [Profil](https://github.com/monschey)
+
+<details>
+<summary> Projektbeschreibung </summary>
+<br>
+
+</details>
+
+<details>
+<summary> genutzte Programme </summary>
+<br>
+
+* [Android Studio](https://developer.android.com/studio) - IDE für JAVA/Kotlin
+* [lucidchart](https://www.lucidchart.com) - Tool für die Erstellung der Diagramme / Charts / ...
+* [Office](https://www.office.com/) - Office Programm
+* [Git](https://git-scm.com/) - Versionskontrolle
+* [Discord](https://discord.com/) - Kommunikationsmittel
+
+</details>
---
+++ app/src/main/java/PME/myfitnessbuddy/view/ui/person/PersonFragment.java
@@ -0,0 +1,41 @@
+package PME.myfitnessbuddy.view.ui.person;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.myfitnessbuddy.R;
+
+import java.util.List;
+import java.util.Observer;
+
+import PME.myfitnessbuddy.model.Person;
+import PME.myfitnessbuddy.storage.Dao.PersonDao;
+import PME.myfitnessbuddy.storage.MyFitnessBuddyDatabase;
+import PME.myfitnessbuddy.view.MainActivity;
+
+public class PersonFragment extends Fragment {
+
+    private PersonViewModel personViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = getLayoutInflater().inflate(R.layout.nav_header_main, container, false);
+
+
+
+        return view;
+    }
+}
