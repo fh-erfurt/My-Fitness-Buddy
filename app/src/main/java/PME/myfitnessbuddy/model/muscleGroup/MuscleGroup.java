f26a948f9e49185930304163f423271d4a4f42af
BenjaminS01 <bswarovsky@gmx.net>
2021-01-06 22:45:58 +0100

    Training create spinner (@strings, @array), list_item_training, list_item_exerciese_withcheckbox bearbeitet, code aufgeräumt (noch nicht komplett)merge

---
+++ app/src/main/java/PME/myfitnessbuddy/model/muscleGroup/MuscleGroup.java
@@ -0,0 +1,168 @@
+package PME.myfitnessbuddy.model.muscleGroup;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.myfitnessbuddy.R;
+
+
+@Entity
+public class MuscleGroup {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "muscleGroupId")
+    private long muscleGroupId;
+
+    /*
+    public long getExerciseId() {
+        return exerciseId;
+    }
+
+    public void setExerciseId(long exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+
+
+    @NonNull
+    @ColumnInfo(name = "exerciseId")
+    private long exerciseId;
+
+
+     */
+    @NonNull
+    @ColumnInfo(name = "designation")
+    private String designation;
+
+    /*
+    @Ignore
+    private List<Exercise> exercises = new ArrayList<>();
+
+
+     */
+    @NonNull
+    public int getProfileImageId() {
+        return profileImageId;
+    }
+
+    public void setProfileImageId(@NonNull int muscleGroup) {
+        this.profileImageId = muscleGroup;
+    }
+
+    public void setProfileImageUrlByString(String muscleGroup) {
+        this.profileImageId = checkImgAndGetId(muscleGroup);
+    }
+
+    @NonNull
+    @ColumnInfo(name = "profile_image_id")
+    private int profileImageId;
+
+
+    @NonNull
+    @ColumnInfo(name = "created")
+    private long created;
+
+    @NonNull
+    @ColumnInfo(name = "modified")
+    private long modified;
+
+    @NonNull
+    @ColumnInfo(name = "version")
+    private int version = 0;
+
+
+
+    public MuscleGroup(@NonNull String designation) {
+        this.designation = designation;
+    }
+
+    public long getMuscleGroupId() {
+        return muscleGroupId;
+    }
+
+    public void setMuscleGroupId(long muscleGroupId) {
+        this.muscleGroupId = muscleGroupId;
+    }
+
+    @NonNull
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(@NonNull String designation) {
+        this.designation = designation;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
+
+    public long getModified() {
+        return modified;
+    }
+
+    public void setModified(long modified) {
+        this.modified = modified;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    private int checkImgAndGetId(String profileImageName) {
+
+        int id;
+
+        switch (profileImageName) {
+            case "Bizeps":
+                id = R.drawable.biceps;
+                break;
+            case "Rücken":
+                id = R.drawable.upper_back;
+                break;
+            case "Brust":
+                id = R.drawable.chest;
+                break;
+            case "Beine":
+                id = R.drawable.quadriceps;
+                break;
+            case "Trizeps":
+                id = R.drawable.triceps;
+                break;
+            case "Wade":
+                id = R.drawable.calves;
+                break;
+            case "Schultern":
+                id = R.drawable.shoulders;
+                break;
+            case "Bauch":
+                id = R.drawable.abs;
+                break;
+            default:
+                id = R.drawable.info;
+                break;
+        }
+
+        return id;
+
+    }
+
+}
---
+++ app/src/main/java/PME/myfitnessbuddy/view/ui/training/SharedViewModel.java
@@ -0,0 +1,34 @@
+package PME.myfitnessbuddy.view.ui.training;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SharedViewModel extends ViewModel {
+
+    private MutableLiveData<CharSequence> trainingsName = new MutableLiveData<>();
+    private MutableLiveData<CharSequence> trainingsCategory = new MutableLiveData<>();
+
+
+    // share data from fragment to fragment
+    public void setTrainingsName(CharSequence input) {
+        trainingsName.setValue(input);
+    }
+
+
+    public LiveData<CharSequence> getTrainingsName() {
+
+        return trainingsName;
+    }
+
+    public void setCategroy(CharSequence input) {
+        trainingsCategory.setValue(input);
+    }
+
+    public LiveData<CharSequence> getCategory() {
+
+        return trainingsCategory;
+    }
+
+
+}
