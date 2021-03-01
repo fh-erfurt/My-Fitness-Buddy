2bfb0a234e444b9843f91ab8f201b306e5d65603
chris <christian.koenig@fh-erfurt.de>
2021-03-01 17:05:20 +0100

    HomeFragment mit Logo

--- app/src/main/java/PME/myfitnessbuddy/view/ui/home/HomeViewModel.java
+++ app/src/main/java/PME/myfitnessbuddy/view/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("My Fitness Buddy Dashboard ");
+        mText.setValue("Dashboard");
     }
 
     public LiveData<String> getText() {
